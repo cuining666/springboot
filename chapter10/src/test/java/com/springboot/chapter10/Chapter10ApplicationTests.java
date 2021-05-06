d84c525e65fc4a740e8444676f97cab9c07d5251
cuining666 <cuining_tarena@outlook.com>
2021-05-06 21:00:50 +0800

    spring boot chapter12 commit

--- chapter10/src/test/java/com/springboot/chapter10/Chapter10ApplicationTests.java
+++ chapter10/src/test/java/com/springboot/chapter10/Chapter10ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.springboot.chapter10;
 
+import com.springboot.chapter10.pojo.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
-class Chapter10ApplicationTests {
+// 使用随机端口启动测试服务
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Chapter10ApplicationTests {
+
+    // REST测试模板，Spring Boot自动提供
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
-    void contextLoads() {
+    public void testGetUser() {
+        // 请求当前启动的服务，请注意URI的缩写
+        User user = template.getForObject("/user/{id}", User.class, 1L);
+        Assert.assertNotNull(user);
     }
 
 }
---
+++ chapter12/src/main/java/com/springboot/chapter12/config/AsyncConfig.java
@@ -0,0 +1,4 @@
+package com.springboot.chapter12.config;
+
+public class AsyncConfig {
+}
