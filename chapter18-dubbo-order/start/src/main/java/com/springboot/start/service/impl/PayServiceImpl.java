5d0b716b05b840e0502bce792e7ff629ce758d12
cuining666 <cuining_tarena@outlook.com>
2021-05-23 14:43:01 +0800

    springboot chapter18 dubbo order service commit

--- chapter18-dubbo-order/start/src/main/java/com/springboot/start/service/impl/PayServiceImpl.java
+++ chapter18-dubbo-order/start/src/main/java/com/springboot/start/service/impl/PayServiceImpl.java
@@ -6,7 +6,6 @@ import com.springboot.start.service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
